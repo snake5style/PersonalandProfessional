5f61b3e1af89e273f0bbf0ba2c0d9962cf9217fb
jamesbond <markasfuller@gmail.com>
2020-09-11 10:03:27 -0400

    adding picture

--- portfolo/personal/urls.py
+++ portfolo/personal/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.personal_index, name="personal_index"),
+    path("about_index", views.about_index, name="about_index"),
 
 ]
--- portfolo/personal/views.py
+++ portfolo/personal/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def personal_index(request):
     return render(request, 'personal_index.html')
+
+def about_index(request):
+    return render(request, 'about_index.html')

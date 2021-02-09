e678f3f751bd0fde4983a6683d2035d96f5961ec
snake5style <markasfuller@gmail.com>
2021-02-09 08:21:23 -0500

    adding comments to futbolallinone

--- portfolo/futbolallinone/views.py
+++ portfolo/futbolallinone/views.py
@@ -10,15 +10,18 @@ import pandas as pd
 from personal_portfolio.settings import DATABASES
 from futbolallinone.models import bund, epl, liga, ligue, serie
 
-
+# Variables db connection
 User = DATABASES['default']['USER']
 Password = DATABASES['default']['PASSWORD']
 Host = DATABASES['default']['HOST']
 Port = DATABASES['default']['PORT']
 Database = DATABASES['default']['NAME']
 
+# Connecting to db
 engine = create_engine("postgresql://" + User + ":" + Password + "@" + Host + ":" + Port + "/" + Database)
 
+#Scraping Futbol data from Skysports league tables
+# List of addresses
 page = [
       'https://www.skysports.com/premier-league-table',
       'https://www.skysports.com/la-liga-table',
@@ -27,10 +30,12 @@ page = [
       'https://www.skysports.com/ligue-1-table'
 ]
 
-
+# Lists for for loop
 league = [ "epl", "liga", "bund", "serie", "ligue", ]
 columns = ['Rank', 'Team', 'Pl', 'W', 'D', 'L', 'F', 'A', 'GD', 'Pts', 'Last 6']
 
+# For loop to scrape data and create
+# pandas dataframe
 for pag, leag in zip(page, league):
    response = requests.get(pag)
    soup = BeautifulSoup(response.text, 'html.parser')
@@ -44,15 +49,20 @@ for pag, leag in zip(page, league):
       teams = [tea.get_text().strip('\n') for tea in content.find_all('td')]
       first_df = pd.DataFrame(teams, columns).T
       first_df.columns=columns
-
+      
+      # Combine both dataframes, then add to db tables
       last_df = pd.concat([last_df,first_df], ignore_index=True)
       last_df.to_sql('{0}'.format(leag), engine, if_exists='replace',index=False)   
-  
+
+# Getting data from db tables 
 bund_table = list(bund.objects.values_list())
 epl_table = list(epl.objects.values_list())
 liga_table = list(liga.objects.values_list())
 ligue_table = list(ligue.objects.values_list())
 serie_table = list(serie.objects.values_list())
+
+# Adding data to dictionary
+# for front-end
 context = {
     'bund_table': bund_table,
     'epl_table': epl_table,
